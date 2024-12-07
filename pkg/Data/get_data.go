d383b3f5c7760904ae618d75d2bd7f5c1b34c2f4
CX330Blake <timchen975@gmail.com>
2024-12-07 23:20:49 +0800

    .

--- pkg/Data/get_data.go
+++ pkg/Data/get_data.go
@@ -11,7 +11,7 @@ func parseInput() {
 func fetchAPI(from string, to string, date string) {
 	// 使用 fmt.Sprintf 格式化 URL
 	url := fmt.Sprintf(
-		"https://tdx.transportdata.tw/api/basic/v3/Rail/TRA/DailyTrainTimetable/OD/Inclusive/%d/to/%d/%s?$format=json",
+		"https://tdx.transportdata.tw/api/basic/v3/Rail/TRA/DailyTrainTimetable/OD/%d/to/%d/%s?$format=json",
 		from,
 		to,
 		date,
